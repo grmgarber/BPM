87d13ca05413b5c10b3fcc33907e01ba633388c4
grmgarber <grmgarber@gmail.com>
2018-04-03 16:09:11 -0400

    add migrations and models

---
+++ app/models/book.rb
@@ -0,0 +1,19 @@
+class Book < ApplicationRecord
+  belongs_to :format
+  has_and_belongs_to_many :authors
+
+  validates :title, presence: true
+  validates :release_date, presence: true
+  validates :price,
+            numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
+  validates :price, presence: true, if: :base_price_required?
+  validates :authors, length: { minimum: 1, message: 'An author is required' }
+
+  MAX_MONTHS_BEFORE_PUB_WITHOUT_PRICE = 2
+
+  private
+
+  def base_price_required?
+    release_date - MAX_MONTHS_BEFORE_PUB_WITHOUT_PRICE.months <= Date.today
+  end
+end
