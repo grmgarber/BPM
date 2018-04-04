a502b679c7970a10d23082b2096fef72ae568ae8
grmgarber <grmgarber@gmail.com>
2018-04-04 10:37:13 -0400

    js moved to books.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,46 +19,6 @@
 // require turbolinks
 //= require_tree .
 
-$(function(){
+$(function() {
     // $(document).foundation();
-    const author_ids = [];
-    function async_update() {
-        setTimeout(
-            function () {
-                $("#author-search").val("");
-                $("#author_ids").val(
-                    $("#authors li").toArray().reduce(
-                        function(m,e) {
-                            return m + $(e).attr("id") + ","
-                        },
-                        ""));
-            },
-            0);
-    }
-
-    $(".date-picker").datepicker();
-
-    $('input#author-search').autocomplete({
-        source: "/authors",
-        minLength: 3,
-        select: function(event,ui) {
-            if (ui.item.id === -1 || author_ids.indexOf(ui.item.id) > -1) {
-                event.preventDefault();
-                return false;
-            } else {
-                $('#authors').append("<li id='" + ui.item.id + "'>" + ui.item.label + "</li>");
-                author_ids.push(ui.item.id);
-                async_update();
-            }
-        }
-
-    });
-    $("#authors").on("click", "li", function (evt) {
-        const idx = author_ids.indexOf(parseInt($(evt.target).attr("id")));
-        if (idx > -1) {
-            author_ids.splice(idx,1);
-        }
-        $(evt.target).remove();
-        async_update();
-    })
 });
